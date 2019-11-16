f8792d038bb1402550c703dc55feb2bddd1f6b27
Adam Hursey <adamhursey24@gmail.com>
2019-11-16 16:00:37 -0700

    added error handling

--- app.js
+++ app.js
@@ -9,8 +9,24 @@ app.use(bodyParser.text());
 
 // GET function used to recieve the request
 app.get("/api", (request, response) => {
-  const text = request.body;
-  response.status(200).send(text);
+ const text = request.body;
+
+ // checks if text is type string
+ if (typeof text === 'string') {
+   response.status(200).send({
+     status: 200,
+     response: processRequest(text)
+   });
+ }
+
+ // if text type is not string then we cannot parse data
+ else {
+   response.status(400).send({
+     status: 400,
+     message:
+       "Invalid request. Please make sure your request has a raw text body."
+   });
+ }
 });
 
 // set PORT where the API will run
@@ -18,3 +34,13 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
+
+// Function to process the CSV and return it in the proper format. Returns String.
+function processRequest(text){
+  let respArray = text.split(",")
+  let splitString = ''
+  respArray.forEach(element => {
+    splitString = splitString + `[${element}]`
+  });
+  return splitString;
+}
